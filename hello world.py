55050c9a7030066dcc28f4b92626f4ca82d7ba31
f-zhuo <49611588+f-zhuo@users.noreply.github.com>
2019-05-14 01:13:49 -0500

    Add files via upload

---
+++ hello world.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 10 23:59:52 2019
+
+@author: chris
+"""
+
+print('hello, world!')
+print("你好,世界！")
+print('你好', '世界')
+print('hello', 'world', sep=', ', end='!')
+print('goodbye, world', end='!\n')
+
+import sys
+print(sys.version)
+print(sys.version_info)
